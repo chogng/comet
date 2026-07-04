223203d658ece5c4c527630bbdbc9263c00e055e
chenchen <56059950+chogng@users.noreply.github.com>
2026-07-04 12:06:27 +0800

    chore: remove web workbench CSS

--- src/cs/workbench/workbench.web.main.ts
+++ src/cs/workbench/workbench.web.main.ts
@@ -1,10 +1,5 @@
 //#region --- workbench common
 import 'cs/workbench/workbench.common.main';
-import 'cs/workbench/workbench.web.main.css';
 //#endregion
 
 // Web entry intentionally excludes desktop-only sandbox/window registrations.
-
-if (typeof document !== 'undefined') {
-  document.body.classList.add('web');
-}
