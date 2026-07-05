77bedf91240a1da94b231a95e0b92960771faf29
lance <56059950+chogng@users.noreply.github.com>
2026-07-04 21:16:58 -0700

    feat: 添加多个新的日志和请求相关文件

--- src/cs/platform/markdown/browser/markdownRenderer.ts
+++ src/cs/platform/markdown/browser/markdownRenderer.ts
@@ -3,14 +3,21 @@
  *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type {
-	IRenderedMarkdown,
-	MarkdownRenderOptions,
-} from 'cs/base/browser/markdownRenderer';
-import { renderMarkdown } from 'cs/base/browser/markdownRenderer';
+import { IRenderedMarkdown, MarkdownRenderOptions, renderMarkdown } from 'cs/base/browser/markdownRenderer';
 import type { IMarkdownString } from 'cs/base/common/htmlContent';
 import { InstantiationType, registerSingleton } from 'cs/platform/instantiation/common/extensions';
 import { createDecorator } from 'cs/platform/instantiation/common/instantiation';
+
+/**
+ * Renders markdown to HTML elements.
+ * 
+ * This interface is intended to be used by clients that want to render markdown content to HTML elements. 
+ * It provides a method to render markdown strings with optional rendering options and an output element.
+ * 
+ * If you want to render markdown content in a standard way, 
+ * prefer using the {@linkcode IMarkdownRendererService} service instead of implementing this interface directly.
+ */
+
 
 export interface IMarkdownRenderer {
 	render(
