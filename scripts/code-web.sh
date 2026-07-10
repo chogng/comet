be910b7e580e83200ffb04c6df44430ff94df958
lance <56059950+chogng@users.noreply.github.com>
2026-07-09 23:58:26 -0700

    chore: add web run action for vite

---
+++ scripts/code-web.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
+	ROOT=$(dirname "$(dirname "$(realpath "$0")")")
+else
+	ROOT=$(dirname "$(dirname "$(readlink -f "$0")")")
+fi
+
+cd "$ROOT"
+
+exec node "$ROOT/node_modules/vite/bin/vite.js" --config "$ROOT/vite.web.config.ts" "$@"
